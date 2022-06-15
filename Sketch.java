4e3ac23bd72579e34c0d0c5e77797b3e06bb5219
Jxz-Jw <jasmine.woo23@ycdsbk12.ca>
2022-06-15 11:00:42 -0400

    hello

--- Sketch.java
+++ Sketch.java
@@ -2,7 +2,6 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -24,14 +23,6 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -43,9 +34,7 @@ public class Sketch extends PApplet {
     draw_section7();
     draw_section8();
 
-    
   }
-
 
   /**
    * Draw the outlines for all sections
@@ -70,14 +59,14 @@ public class Sketch extends PApplet {
   /**
    * draws the bottom left section
    */
-  public void draw_section1(){
+  public void draw_section1(){ // 30 by 30 white blocks
     int intX = 0;
     int intY = 0;
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 3 + intRow;
+        intY = 300 + 3 + intColumn;
 
         fill(255);
         noStroke();
@@ -87,51 +76,151 @@ public class Sketch extends PApplet {
     }
   }
 
-  /**
-   * Use the modulus operator and an if statement to select the color
-   * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
-   */
-  public void draw_section2(){
+  public void draw_section2(){ // vertically alternating white and black blocks
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 300 + 3 + intRow;
+        intY = 300 + 3 + intColumn;
+      
+        if ((intRow/10) % 2 == 0)
+        {
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+        else 
+        {
+          fill(0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+
+      }
+    }
   }
 
-  /**
-   * Use the modulus operator and an if/else statement to select the color.
-   * Don't use multiple 'if' statements.
-   */
-  public void draw_section3(){
 
+  public void draw_section3(){ // horizontally alternating white and black blocks 
+    int intX = 0;
+    int intY = 0;
+
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 600 + 3 + intRow;
+        intY = 300 + 3 + intColumn;
+
+        if ((intColumn/10) % 2 == 0)
+        {
+          fill(0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+        else 
+        {
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+
+      }
+    }
   }
 
-  /**
-   * Use the modulus operator and just one 'if' statement to select the color.
-   */
-  public void draw_section4(){
+  public void draw_section4(){ // black and white block grid with black bordering
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 900 + 3 + intRow;
+        intY = 300 + 3 + intColumn;
+
+        if ((intRow/10) % 2 != 0 || (intColumn/10) % 2 == 0)
+        {
+          fill(0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+        else 
+        {
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+
+      }
+    }
   }
 
-  /**
-   * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
-   */
-  public void draw_section5(){
-
+  public void draw_section5(){ // bottom right diagonal half of white blocks
+    int intX = 0;
+    int intY = 0;
+    
+    for(int intColumn = 0; intColumn < 300; intColumn += 10){
+      for(int intRow = 300 - intColumn; intRow < 300; intRow += 10){
+        intX = 3 + intRow;
+        intY = 3 + intColumn;
+        
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      
+      }
+    }
   }
 
-  public void draw_section6(){
-
+  public void draw_section6(){ // bottom left diagonal half of white blocks
+    int intX = 0;
+    int intY = 0;
+    
+    for(int intColumn = 0; intColumn < 300; intColumn += 10){
+      for(int intRow = 0; intRow <= intColumn; intRow += 10){
+        intX = 300 + 3 + intRow;
+        intY = 3 + intColumn;
+        
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      
+      }
+    }
   }
 
-  public void draw_section7(){
-
+  public void draw_section7(){ // upper left diagonal half of white blocks
+    int intX = 0;
+    int intY = 0;
+    
+    for(int intColumn = 0; intColumn < 300; intColumn += 10){
+      for(int intRow = 290 - intColumn ; intRow >= 0; intRow -= 10){
+        intX = 600 + 3 + intRow;
+        intY = 3 + intColumn;
+        
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
   
-  public void draw_section8(){
+  public void draw_section8(){ // upper right diagonal half of white blocks
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn <= intRow; intColumn += 10){
+        intX = 900 + 3 + intRow;
+        intY = 3 + intColumn;
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+
+      }
+    }
+    
   }
-
-
-
-
-
 
 }
